491b1309e614cfafda650774d19503be70d832ac
oyuerdenem <55132542+oyuerdenem@users.noreply.github.com>
2023-12-18 02:32:17 +0800

    Nar, Od hoyr nemsen, init function nemsen.

--- main.cpp
+++ main.cpp
@@ -12,143 +12,74 @@ static GLfloat xRot = 0.0f;
 static GLfloat yRot = 0.0f;
 float scale = 1.0f;
 
+GLfloat sunRadius = 0.3f;
+
 GLuint ObjectList;
 
-void init(void)
-{
-   GLfloat ambient[] = {0.0, 0.0, 0.0, 1.0};
-   GLfloat diffuse[] = {1.0, 1.0, 1.0, 1.0};
-   GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
-   GLfloat position[] = {0.0, 3.0, 3.0, 0.0};
+void ReduceToUnit(float vector[3]) {
+    float length;
+    length = (float)sqrt((vector[0] * vector[0]) +
+                         (vector[1] * vector[1]) +
+                         (vector[2] * vector[2]));
+    if (length == 0.0f)
+        length = 1.0f;
 
-   GLfloat lmodel_ambient[] = {0.2, 0.2, 0.2, 1.0};
-   GLfloat local_view[] = {0.0};
-
-   glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
-   glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
-   glLightfv(GL_LIGHT0, GL_POSITION, position);
-   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
-   glLightModelfv(GL_LIGHT_MODEL_LOCAL_VIEWER, local_view);
-
-   glFrontFace(GL_CW);
-   glEnable(GL_LIGHTING);
-   glEnable(GL_LIGHT0);
-   glEnable(GL_AUTO_NORMAL);
-   glEnable(GL_NORMALIZE);
-   glEnable(GL_DEPTH_TEST);
-/*  be efficient--make teapot display list  */
-   /*ObjectList = glGenLists(1);
-   glNewList (ObjectList, GL_COMPILE);
-   glutSolidTeapot(1.0);
-   glEndList ();*/
+    vector[0] /= length;
+    vector[1] /= length;
+    vector[2] /= length;
 }
+void calcNormal(float v[3][3], float out[3]) {
+    float v1[3], v2[3];
+    static const int x = 0;
+    static const int y = 1;
+    static const int z = 2;
 
-void renderTeapot(GLfloat x, GLfloat y,
-   GLfloat ambr, GLfloat ambg, GLfloat ambb,
-   GLfloat difr, GLfloat difg, GLfloat difb,
-   GLfloat specr, GLfloat specg, GLfloat specb, GLfloat shine)
-{
-   GLfloat mat[4];
+    v1[x] = v[0][x] - v[1][x];
+    v1[y] = v[0][y] - v[1][y];
+    v1[z] = v[0][z] - v[1][z];
 
-   glPushMatrix();
-   glTranslatef(x, y, 0.0);
+    v2[x] = v[1][x] - v[2][x];
+    v2[y] = v[1][y] - v[2][y];
+    v2[z] = v[1][z] - v[2][z];
 
-   mat[0] = ambr;
-   mat[1] = ambg;
-   mat[2] = ambb;
-   mat[3] = 1.0;
-   glMaterialfv(GL_FRONT, GL_AMBIENT, mat);
+    out[x] = v1[y] * v2[z] - v1[z] * v2[y];
+    out[y] = v1[z] * v2[x] - v1[x] * v2[z];
+    out[z] = v1[x] * v2[y] - v1[y] * v2[x];
 
-   mat[0] = difr;
-   mat[1] = difg;
-   mat[2] = difb;
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat);
-
-   mat[0] = specr;
-   mat[1] = specg;
-   mat[2] = specb;
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat);
-
-   glMaterialf(GL_FRONT, GL_SHININESS, shine * 128.0);
-   //glCallList(teapotList);
-   glPopMatrix();
+    ReduceToUnit(out);
 }
+void SetupRC(){
+    GLfloat  whiteLight[] = { 0.05f, 0.05f, 0.05f, 1.0f };
+    GLfloat  sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+    GLfloat	 lightPos[] = { -10.f, 5.0f, 5.0f, 1.0f };
 
-/*void display(void)
-{
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   renderTeapot(2.0, 17.0, 0.0215, 0.1745, 0.0215,
-      0.07568, 0.61424, 0.07568, 0.633, 0.727811, 0.633, 0.6);
-   renderTeapot(2.0, 14.0, 0.135, 0.2225, 0.1575,
-      0.54, 0.89, 0.63, 0.316228, 0.316228, 0.316228, 0.1);
-   renderTeapot(2.0, 11.0, 0.05375, 0.05, 0.06625,
-      0.18275, 0.17, 0.22525, 0.332741, 0.328634, 0.346435, 0.3);
-   renderTeapot(2.0, 8.0, 0.25, 0.20725, 0.20725,
-      1, 0.829, 0.829, 0.296648, 0.296648, 0.296648, 0.088);
-   renderTeapot(2.0, 5.0, 0.1745, 0.01175, 0.01175,
-      0.61424, 0.04136, 0.04136, 0.727811, 0.626959, 0.626959, 0.6);
-   renderTeapot(2.0, 2.0, 0.1, 0.18725, 0.1745,
-      0.396, 0.74151, 0.69102, 0.297254, 0.30829, 0.306678, 0.1);
-   renderTeapot(6.0, 17.0, 0.329412, 0.223529, 0.027451,
-      0.780392, 0.568627, 0.113725, 0.992157, 0.941176, 0.807843,
-      0.21794872);
-   renderTeapot(6.0, 14.0, 0.2125, 0.1275, 0.054,
-      0.714, 0.4284, 0.18144, 0.393548, 0.271906, 0.166721, 0.2);
-   renderTeapot(6.0, 11.0, 0.25, 0.25, 0.25,
-      0.4, 0.4, 0.4, 0.774597, 0.774597, 0.774597, 0.6);
-   renderTeapot(6.0, 8.0, 0.19125, 0.0735, 0.0225,
-      0.7038, 0.27048, 0.0828, 0.256777, 0.137622, 0.086014, 0.1);
-   renderTeapot(6.0, 5.0, 0.24725, 0.1995, 0.0745,
-      0.75164, 0.60648, 0.22648, 0.628281, 0.555802, 0.366065, 0.4);
-   renderTeapot(6.0, 2.0, 0.19225, 0.19225, 0.19225,
-      0.50754, 0.50754, 0.50754, 0.508273, 0.508273, 0.508273, 0.4);
-   renderTeapot(10.0, 17.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01,
-      0.50, 0.50, 0.50, .25);
-   renderTeapot(10.0, 14.0, 0.0, 0.1, 0.06, 0.0, 0.50980392, 0.50980392,
-      0.50196078, 0.50196078, 0.50196078, .25);
-   renderTeapot(10.0, 11.0, 0.0, 0.0, 0.0,
-      0.1, 0.35, 0.1, 0.45, 0.55, 0.45, .25);
-   renderTeapot(10.0, 8.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0,
-      0.7, 0.6, 0.6, .25);
-   renderTeapot(10.0, 5.0, 0.0, 0.0, 0.0, 0.55, 0.55, 0.55,
-      0.70, 0.70, 0.70, .25);
-   renderTeapot(10.0, 2.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0,
-      0.60, 0.60, 0.50, .25);
-   renderTeapot(14.0, 17.0, 0.02, 0.02, 0.02, 0.01, 0.01, 0.01,
-      0.4, 0.4, 0.4, .078125);
-   renderTeapot(14.0, 14.0, 0.0, 0.05, 0.05, 0.4, 0.5, 0.5,
-      0.04, 0.7, 0.7, .078125);
-   renderTeapot(14.0, 11.0, 0.0, 0.05, 0.0, 0.4, 0.5, 0.4,
-      0.04, 0.7, 0.04, .078125);
-   renderTeapot(14.0, 8.0, 0.05, 0.0, 0.0, 0.5, 0.4, 0.4,
-      0.7, 0.04, 0.04, .078125);
-   renderTeapot(14.0, 5.0, 0.05, 0.05, 0.05, 0.5, 0.5, 0.5,
-      0.7, 0.7, 0.7, .078125);
-   renderTeapot(14.0, 2.0, 0.05, 0.05, 0.0, 0.5, 0.5, 0.4,
-      0.7, 0.7, 0.04, .078125);
-   glFlush();
-}*/
+    glEnable(GL_DEPTH_TEST);
+    glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
 
-void initLighting() {
+    // Enable lighting
     glEnable(GL_LIGHTING);
 
-    GLfloat light_direction[] = {1.0, 1.0, 1.0, 0.0};
-
-    GLfloat light_ambient[] = {1.0, 1.0, 0.0, 1.0}; // Yellow ambient light
-    GLfloat light_diffuse[] = {1.0, 1.0, 0.0, 1.0}; // Yellow diffuse light
-    GLfloat light_specular[] = {1.0, 1.0, 0.0, 1.0}; // Yellow specular light
-
-    glLightfv(GL_LIGHT0, GL_POSITION, light_direction);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-
+    // Setup and enable light 0
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,whiteLight);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,sourceLight);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,sourceLight);
+    glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
     glEnable(GL_LIGHT0);
+
+    // Enable color tracking
+    glEnable(GL_COLOR_MATERIAL);
+
+    // Set Material properties to follow glColor values
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+    glClearColor(0.9078f, 0.9219f, 0.9529f, 0.9f); // background color - blue
 }
 
-void RenderStar(void) {
-    float rad_big = 30.0f;
-    float rad_small = 15.0f;
+void drawStar() {
+    glColor3f(1.0, 227/255.0, 130/255.0);
+    float rad_big = 0.3f;
+    float rad_small = 0.1f;
 
     //ОДНЫ БҮХ ӨНЦГИЙН РАДИУСЫГ ХАДГАЛНА.
     float angles[10];
@@ -174,16 +105,16 @@ void RenderStar(void) {
         float normal_tr1[3], normal_tr2[3];
         float vertices_tr1[3][3] = {{ -x,  -y, 0},
                                     {-x2, -y2, 0},
-                                    { 0,  0, 10}};
+                                    { 0,  0, 0.1}};
         float vertices_tr2[3][3] = {{-x1, -y1, 0},
                                     { -x,  -y, 0},
-                                    { 0,  0, 10}};
+                                    { 0,  0, 0.1}};
 
-        //calcNormal(vertices_tr1, normal_tr1);
-        //calcNormal(vertices_tr2, normal_tr2);
+        calcNormal(vertices_tr1, normal_tr1);
+        calcNormal(vertices_tr2, normal_tr2);
 
-        //glNormal3fv(normal_tr1);
-        //glNormal3fv(normal_tr2);
+        glNormal3fv(normal_tr1);
+        glNormal3fv(normal_tr2);
 
         glVertex3fv(vertices_tr1[0]);
         glVertex3fv(vertices_tr1[1]);
@@ -215,10 +146,10 @@ void RenderStar(void) {
         float normal_tr1[3], normal_tr2[3];
         float vertices_tr1[3][3] = {{ x,  y, 0},
                                     {x2, y2, 0},
-                                    { 0,  0, 10}};
+                                    { 0,  0, 0.1}};
         float vertices_tr2[3][3] = {{x1, y1, 0},
                                     { x,  y, 0},
-                                    { 0,  0, 10}};
+                                    { 0,  0, 0.1}};
 
         //calcNormal(vertices_tr1, normal_tr1);
         //calcNormal(vertices_tr2, normal_tr2);
@@ -237,48 +168,67 @@ void RenderStar(void) {
         glEnd();
     }
 }
-
-void drawSunAndRadiation() {
+void drawTree(){
     GLUquadric* quadric = gluNewQuadric();
     gluQuadricNormals(quadric, GLU_SMOOTH);
+    glColor3f(78/255.0, 149/255.0, 37/255.0); // green color
 
-    //glColor3f(1.0f, 1.0f, 0.0f);// Sun color (yellow)
-    glColor3f(78/255.0, 149/255.0, 37/255.0);
-
-    // Draw the sun
-    glPushMatrix();
-    glTranslatef(0, 1, -3);
-    glRotatef(xRot, 1.0, 0.0, 0.0);
-    glRotatef(yRot, 0.0, 1.0, 0.0);
-    glScalef(scale, scale, scale);
-    glRotatef(90.0, 1.0, 0.0, 0.0);
-    //gluSphere(quadric, sunRadius, 20, 20);
+    //Layer 1 - Top
+    glTranslatef(0, 0, 0.5);
+    gluCylinder(quadric, 0, 0.05, 0, 100, 1);
     gluCylinder(quadric, 0.05, 0.4, 0.8, 100, 1);
-
+    //Layer 2
     glTranslatef(0, 0, 0.5);
     gluCylinder(quadric, 0.05*2 ,0.4*1.35, 0.8, 100, 1);
-
-    glTranslatef(0, 0, 0.5);
+    glTranslatef(0, 0, 0.3);
+    gluCylinder(quadric, 0.4, 0, 0, 100, 1);
+    //Layer 3
+    glTranslatef(0, 0, 0.2);
     gluCylinder(quadric, 0.05*2*2, 0.4*1.35*1.35, 0.8, 100, 1);
-
-    glTranslatef(0, 0, 0.5);
+    glTranslatef(0, 0, 0.3);
+    gluCylinder(quadric, 0.4*1.35, 0, 0, 100, 1);
+    //Layer 4
+    glTranslatef(0, 0, 0.2);
     gluCylinder(quadric, 0.05*2*2*1.5, 0.4*1.35*1.35*1.25, 0.8, 100, 1);
-
-    glTranslatef(0, 0, 0.5);
+    glTranslatef(0, 0, 0.3);
+    gluCylinder(quadric, 0.4*1.35*1.35, 0, 0, 100, 1);
+    //Layer 5
+    glTranslatef(0, 0, 0.2);
     gluCylinder(quadric, 0.05*2*2*1.5*1.1, 0.4*1.35*1.35*1.25*1.2, 0.8, 100, 1);
+    glTranslatef(0, 0, 0.3);
+    gluCylinder(quadric, 0.4*1.35*1.35*1.25, 0, 0, 100, 1);
+    glTranslatef(0, 0, 0.5);
+    gluCylinder(quadric, 0.4*1.35*1.35*1.25*1.2, 0, 0, 100, 1);
 
+    //Bottom - Wood
+    glColor3f(108/255.0, 52/255.0, 40/255.0);
     glTranslatef(0, 0, -2);
-    gluCylinder(quadric, 0.05, 0.1, 3.4, 100, 1);
-
-    glTranslatef(0, 0, -0.5);
-    glColor3f(206/255.0, 230/255.0, 243/255.0);
-
-    RenderStar();
+    gluCylinder(quadric, 0.05, 0.1, 3, 100, 1);
+    glTranslatef(0, 0, 2);
+    gluCylinder(quadric, 0.1, 0, 0, 100, 1);
 
     glPopMatrix();
 
+    gluDeleteQuadric(quadric);
+}
+void drawSun(){
+    GLUquadric* quadric = gluNewQuadric();
+    gluQuadricNormals(quadric, GLU_SMOOTH);
+
+    glColor3f(1.0f, 1.0f, 0.0f); // Sun color (yellow)
+
+
+    // Draw the sun
+    glPushMatrix();
+    glRotatef(xRot, 1.0, 0.0, 0.0);
+    glRotatef(yRot, 0.0, 1.0, 0.0);
+    glScalef(0.5, 1.0, 1.0);
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+    gluSphere(quadric, sunRadius, 20, 20);
+    glPopMatrix();
+
     // Draw solar radiation
-    /*glPushMatrix();
+    glPushMatrix();
     glRotatef(xRot, 1.0, 0.0, 0.0);
     glRotatef(yRot, 0.0, 1.0, 0.0);
     for (int i = 0; i < 8; i++) {
@@ -289,22 +239,43 @@ void drawSunAndRadiation() {
         gluCylinder(quadric, sunRadius/3, 0.0, sunRadius * 2.5, 20, 20);
         glPopMatrix();
     }
-    glPopMatrix();*/
+    glPopMatrix();
+
+    gluDeleteQuadric(quadric);
+}
+void Render() {
+    GLUquadric* quadric = gluNewQuadric();
+    gluQuadricNormals(quadric, GLU_SMOOTH);
+
+    glTranslatef(0, 0, -3);
+    glRotatef(xRot, 1.0, 0.0, 0.0);
+    glRotatef(yRot, 0.0, 1.0, 0.0);
+    glScalef(scale, scale, scale);
+    //glRotatef(90.0, 1.0, 0.0, 0.0);
+
+    glTranslatef(0, 1, 0);
+    glRotatef(90, 0.0, 0.0, 1.0);
+    //glRotatef(90, 0.0, 1.0, 0.0);
+    drawStar();
+
+    glRotatef(270, 0.0, -1.0, 0.0);
+    glTranslatef(0.0, 0.0, -0.3);
+    drawTree();
+
+    glTranslatef(-5, 2, -5);
+    drawSun();
 
     gluDeleteQuadric(quadric);
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
     glTranslatef(0.0, 0.0, -5.0);
 
-    initLighting();
-
-    drawSunAndRadiation();
+    Render();
 
     glutSwapBuffers();
 }
@@ -316,21 +287,6 @@ void reshape(int w, int h) {
     gluPerspective(45.0, (double)w / (double)h, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
 }
-
-
-/*void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-    case '+':
-        sunRadius += 0.1f;
-        break;
-    case '-':
-        sunRadius -= 0.1f;
-        if (sunRadius < 0.1f) sunRadius = 0.1f;
-        break;
-    }
-    glutPostRedisplay();
-}*/
-
 
 void keyboard(unsigned char key, int x, int y){
     switch(key){
@@ -398,7 +354,9 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 800);
-    glutCreateWindow("Solar Radiation Example");
+    glutCreateWindow("Christmas");
+
+    glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -407,7 +365,7 @@ int main(int argc, char** argv) {
     glutMotionFunc(motion);
     glutMouseFunc(mouse);
 
-    glEnable(GL_DEPTH_TEST);
+    SetupRC();
 
     glutMainLoop();
     return 0;
